765b27b8141a0a75c3eea1f90f1691115cf4a054
Harsh Trivedi <Harsh10102000@gmail.com>
2022-07-19 20:42:53 +0530

    till product page

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+     <p>Fakeshop app 2022&copy;</p>
+    </div>
+  )
+}
+
+export default Footer
+
---
+++ src/components/Products.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import '../App.css';
+import {useState} from 'react';
+import {useEffect} from 'react';
+import axios from "axios";
+import { Link } from 'react-router-dom';
+ import ProductDetails from './components/ProductDetails';
+
+const Products = () => {
+    const [loading, setLoading] = useState(false);
+    const [data, setData] = useState([]);
+  
+    useEffect(() => {
+      setLoading(true);
+      axios({
+        method: "GET",
+        url: "https://fakestoreapi.com/products",
+      })
+        .then((res) => {
+          console.log(res.data);
+          setData(res.data);
+        })
+        .catch((e) => console.log(e))
+        .finally(() => setLoading(false));
+    }, []);
+  
+    return (
+      <div className="products-container">
+        {loading && (
+          <div>
+            {" "}
+            <h1>Loading...</h1>
+          </div>
+        )}
+  
+        {data.map((product)=> ( 
+            <div key={product.id} className="card">
+             <div onClick={<ProducDetails/>} ><img src={product.image} alt="#"/></div>
+             <div className="card-description">
+                 <h6>{product.title}</h6>
+                 <h6>{`Price: ${product.price}`}</h6>
+                 <h6>{`Category: ${product.category}`}</h6>
+                 
+             </div>
+            </div>
+        ))}
+      </div>
+    );
+  };
+export default Products;
